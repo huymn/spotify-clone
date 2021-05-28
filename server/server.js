f9b176d363a2f94afd4ae3d1df76657596c97f24
nguyenhu21 <35584482+nguyenhu21@users.noreply.github.com>
2021-05-28 01:45:20 -0700

    added refresh token feature

--- server/server.js
+++ server/server.js
@@ -1,7 +1,33 @@
 const express = require('express');
+const cors = require('cors');
 const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/refresh', (req, res) => {
+    const refreshToken = req.body.refreshToken;
+    const spotifyWebApi = new SpotifyWebApi({
+        redirectUri: 'http://localhost:3000',
+        clientId: 'd6e018be7f52406b9eb6f98cbd4f9ceb',
+        clientSecret: '09ac428a59be4f8182cf42d5b5f7e12a',
+        refreshToken,
+    })
+
+    spotifyWebApi
+        .refreshAccessToken().
+        then((data) => {
+            res.json({
+                accessToken: data.body.accessToken,
+                expiresIn: data.body.expiresIn
+            })
+        })
+        .catch(() => {
+            res.sendStatus(400);
+        })
+})
 
 app.post('/login', (req, res) => {
     const code = req.body.code;
@@ -24,3 +50,5 @@ app.post('/login', (req, res) => {
             res.sendStatus(400);
         })
 })
+
+app.listen(3001);
